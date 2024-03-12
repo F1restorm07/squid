7bd2550d743ead8e7a7e7715581d656da89b7c5d
F1restorm <luke.n.gardner@icloud.com>
2024-03-11 23:16:03 -0600

    rewrite basic url parsing + add percent encoding

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,40 @@
 #![no_std]
 
 extern crate alloc;
-use alloc::vec::Vec;
+use core::ops::Range;
 use no_std_net::{ Ipv4Addr, Ipv6Addr };
+use bytes::{ Bytes, BytesMut, Buf };
+use percent_encoding::{ CONTROLS, AsciiSet, percent_encode };
 
-pub struct Url<'u> {
-    scheme: Scheme<'u>,
-    authority: Option<Authority<'u>>,
-    host: Option<Host<'u>>,
-    port: Option<u16>,
-    // TODO: change to a static array or slice
-    path: Option<Vec<&'u str>>,
-    query: Option<Vec<Query<'u>>>,
-    fragment: Option<&'u str>,
+const FRAGMENT_ENCODE: &AsciiSet = &CONTROLS.add(b' ').add(b'"').add(b'<').add(b'>').add(b'`');
+const QUERY_ENCODE: &AsciiSet = &CONTROLS.add(b' ').add(b'"').add(b'<').add(b'>').add(b'#');
+const SPECIAL_QUERY_ENCODE: &AsciiSet = &QUERY_ENCODE.add(b'\'');
+const PATH_ENCODE: &AsciiSet = &QUERY_ENCODE.add(b'?').add(b'`').add(b'{').add(b'}');
+const USERINFO_ENCODE: &AsciiSet = &PATH_ENCODE
+    .add(b'/').add(b':').add(b';').add(b'=').add(b'@').add(b'[').add(b'^').add(b'|');
+
+pub struct Url {
+    percent_encoded: Bytes,
+    scheme: Range<usize>,
+    authority: Range<usize>,
+    host: Range<usize>,
+    // port: Range<usize>,
+    path: Range<usize>,
+    query: Range<usize>,
+    fragment: Range<usize>,
+}
+
+impl core::str::FromStr for Url {
+    type Err = ParseError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        parse_url(s)
+    }
+}
+
+impl From<&str> for Url {
+    fn from(value: &str) -> Self {
+        parse_url(value).unwrap()
+    }
 }
 
 pub enum Scheme<'s> {
@@ -46,76 +68,42 @@ pub struct Query<'q>(&'q str, &'q str);
 #[derive(Debug)]
 pub enum ParseError {}
 
-pub fn parse_url(input: &'_ str) -> Result<Url<'_>, ParseError> {
-    let (scheme, rest) = parse_scheme(input).unwrap();
-    let (authority, rest) = parse_authority(rest).unwrap();
-    let (host, rest) = parse_host(rest).unwrap();
-    let (port, rest) = parse_port(rest).unwrap();
-    let port = if port.is_none() {
-      match scheme {
-        Scheme::Http | Scheme::Ws => Some(80),
-        Scheme::Https | Scheme::Wss => Some(443),
-        Scheme::Ftp => Some(21),
-        Scheme::File | Scheme::Other(_) => None
-        }
-    } else { port };
-    let (path, rest) = parse_path(rest).unwrap();
-    let (query, fragment) = parse_query(rest).unwrap();
-
-    Ok(Url { scheme, authority, host, port, path, query, fragment })
+// TODO: make this return a &str
+fn url_percent_encode(input: &'_ str, encoding: &'static AsciiSet) -> Bytes {
+    let mut buf = BytesMut::with_capacity(input.len());
+    buf.extend(percent_encode(input.as_bytes(), encoding).flat_map(|s| s.as_bytes()));
+    buf.freeze()
 }
 
-fn parse_scheme(input: &'_ str) -> Result<(Scheme<'_>, &'_ str), ParseError> {
-    let input = input.split_once(':').unwrap();
-    let scheme = match input.0 {
-        "http" => Scheme::Http,
-        "https" => Scheme::Https,
-        "file" => Scheme::File,
-        "ftp" => Scheme::Ftp,
-        "ws" => Scheme::Ws,
-        "wss" => Scheme::Wss,
-        s => Scheme::Other(s)
-    };
-    Ok((scheme, input.1.strip_prefix("//").unwrap_or("")))
-}
+pub fn parse_url(input: &'_ str) -> Result<Url, ParseError> {
+    let scheme_end = input.find(':').unwrap_or(0);
+    let authority_end = input[scheme_end..].find('@').unwrap_or(0);
+    let host_end = input[authority_end..].find([':', '/']).unwrap_or(scheme_end);
+    // let port_end = input[host_end..host_end+5].find('/').unwrap_or(host_end);
+    // let path_end = input[host_end..].find('?').unwrap_or(port_end);
+    let path_end = input[host_end..].find('?').unwrap_or(host_end);
+    let query_end = input[path_end..].find('#').unwrap_or(path_end);
 
-fn parse_authority(input: &'_ str) -> Result<(Option<Authority>, &'_ str), ParseError> {
-    let input = input.split_once('@').unwrap_or(("", input));
-    if input.0.is_empty() { return Ok((None, input.1)) }
+    let mut percent_encoded = BytesMut::with_capacity(input.len());
+    percent_encoded.extend(url_percent_encode(&input[..scheme_end], CONTROLS)
+        .chain(url_percent_encode(&input[scheme_end..authority_end], USERINFO_ENCODE))
+        .chain(url_percent_encode(&input[authority_end..host_end], CONTROLS))
+        .chain(
+            input[host_end..path_end].split('/')
+                .flat_map(|s| url_percent_encode(s, PATH_ENCODE).slice(..)).collect::<Bytes>()
+        ));
+    let percent_encoded = percent_encoded.freeze();
+    let percent_encoded_len = percent_encoded.len();
 
-    let (user, password) = input.0.split_once(':').unwrap();
-    Ok((Some(Authority { user, password }), input.1))
-}
-
-fn parse_host(input: &'_ str) -> Result<(Option<Host<'_>>, &'_ str), ParseError> {
-    let input = input.split_once('/').unwrap_or(("", input));
-    if input.0.is_empty() { return Ok((None, input.1)) }
-
-    let (hostname, domain) = input.0.split_once('.').unwrap();
-    Ok((Some(Host { name: Hostname::DnsDomain(hostname), domain: Some(domain) }), input.1))
-}
-
-fn parse_port(input: &'_ str) -> Result<(Option<u16>, &'_ str), ParseError> {
-    let input = input.split_once('/').unwrap_or(("", input));
-    if input.0.is_empty() { return Ok((None, input.1)) }
-
-    Ok((Some(input.0.parse().unwrap()), input.1))
-}
-
-fn parse_path(input: &'_ str) -> Result<(Option<Vec<&'_ str>>, &'_ str), ParseError> {
-    let input = input.split_once('?').unwrap_or(("", input));
-    if input.0.is_empty() { return Ok((None, input.1)) }
-
-    // ??: how do i collect directly into a slice
-    let path = input.0.split("/").collect::<Vec<_>>();
-    Ok((Some(path), input.1))
-}
-
-fn parse_query(input: &'_ str) -> Result<(Option<Vec<Query<'_>>>, Option<&'_ str>), ParseError> {
-    let input = input.split_once('#').unwrap_or((input, ""));
-    let query = input.0.split(",").map(|q| {
-        let q = q.split_once('=').unwrap();
-        Query(q.0, q.1)
-    }).collect::<Vec<_>>();
-    Ok((Some(query), if input.1.len() > 1 { Some(input.1) } else { None }))
+    Ok(Url {
+        percent_encoded,
+        scheme: 0..scheme_end,
+        authority: scheme_end..authority_end,
+        host: authority_end..host_end,
+        // port: host_end..port_end,
+        // path: port_end..path_end,
+        path: host_end..path_end,
+        query: path_end..query_end,
+        fragment: query_end..percent_encoded_len
+    })
 }
